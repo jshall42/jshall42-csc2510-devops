2ed6d2b7b76d7a77c1a0cad51530f79d59d74844
Jacob Hall <jsamh12@gmail.com>
2024-10-29 00:08:33 +0000

    Add ASCII art script with colored output

---
+++ ascii_art.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' 
+
+  echo -e "${RED}    /\    ${NC}"
+  echo -e "${RED}   /  \   ${NC}"
+echo -e "${GREEN}  /0__0\  ${NC}"
+ echo -e "${BLUE} /      \ ${NC}"
+ echo -e "${BLUE}/        \ "
+echo -e "${GREEN}----------${NC}"
+echo -e "${GREEN} Little Guy  ${NC}"
